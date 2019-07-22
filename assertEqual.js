8a61e72b2443378e5c03680a1ceeda24ffc3e749
Arturas Zuta <arturaszuta@gmail.com>
2019-07-22 17:20:23 +0000

    Add assertEqual function for comparing values

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Watermelon", "Watermelon");
+assertEqual(1,1);
+assertEqual(2,4);
