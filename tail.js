5c8ecdcaf0141b6b22206453df63bb6940355b17
Arturas Zuta <arturaszuta@gmail.com>
2019-07-22 18:27:42 +0000

    Solve lint issues

--- tail.js
+++ tail.js
@@ -1,15 +1,15 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`❌ ❌ ❌  Assertion failed: ${actual} !== ${expected}`);
-    }
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${actual} !== ${expected}`);
+  }
 };
 
 const tail = function(array) {
   const newArray = array.slice(1, array.length);
   return newArray;
-}
+};
 
 console.log(tail([1]));
 console.log(tail([]));
@@ -27,7 +27,7 @@ assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-const testCase2 = tail([1,5,6,7,8])
+const testCase2 = tail([1,5,6,7,8]);
 assertEqual(testCase2.length, 4); // ensure we get back two elements
 assertEqual(testCase2[0], 5); // ensure first element is "Lighthouse"
 assertEqual(testCase2[1], 6); // ensure second element is "Labs"
