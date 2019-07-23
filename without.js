867730b535e8df1ea080595d360234f18732cdce
Arturas Zuta <arturaszuta@gmail.com>
2019-07-23 20:07:57 +0000

    Create function to remove elements from array give another array as arguments

--- without.js
+++ without.js
@@ -21,3 +21,23 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`❌ ❌ ❌  Assertion failed: ${array1} !== ${array2}`);
   }
 };
+
+const without = function(source, itemsToRemove) {
+  let answerArray = source.filter((element) => {
+    if (itemsToRemove.includes(element)) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  return answerArray;
+};
+
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without(["1", "2", "3", "bob", "thomas", true], [1, 2, "3"]));
+console.log(without(["1", "2", "3", "bob", "thomas", true], [1, 2, "3", "thomas"]));
+console.log(without(["1", "2", "3", "bob", "thomas", true], [1, 2, "3", true]));
+console.log(without(["1", "2", "3", "bob", "thomas", true], [1, 2, "3"]));
+
+console.log(assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ['1', '2']));
+console.log(assertArraysEqual(without(["1", "2", "3", "bob", "thomas", true], [1, 2, "3", true, "thomas"]), ["1", "2", "bob"]));
