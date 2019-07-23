ffa55cf736af0b41a360f957c0d86afe192c4106
Arturas Zuta <arturaszuta@gmail.com>
2019-07-23 17:43:01 +0000

    Add function to log messages when comparing arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1,array2);
+  if (result) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+
+assertArraysEqual(['hi', 'my', 'name'], ['hi', 'my', 3, 'name']);
+assertArraysEqual([1,2,34,5,'bob'], [1,2,34,5,'bob']);
+assertArraysEqual(['hi', 2, false], ['hi', 2, false]);
