a1758148600856df1734101987779594b95a2f0b
Arturas Zuta <arturaszuta@gmail.com>
2019-07-24 12:59:26 +0000

    Create function to count unique letters in passed word/phrase

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(word) {
+  let answerObject = {};
+  const noSpaces = word.split(' ').join('').toLowerCase();
+  for (const letter of noSpaces) {
+    if (answerObject[letter]) {
+      answerObject[letter] += 1;
+    } else {
+      answerObject[letter] = 1;
+    }
+  }
+  return answerObject;
+};
+
+const test = countLetters('This is sparta');
+console.log(assertEqual(test['s'], 3));
+
