df26bca356b42dee3d17ce269e49ef597e27f9dd
Arturas Zuta <arturaszuta@gmail.com>
2019-07-25 16:28:34 +0000

    Add function findKey in an object and call a callback function on it, return first object which returns true from the callback funciton

---
+++ findKeys.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const objArray = Object.keys(object);
+  for (let arrayElement of objArray) {
+    if (callback(object[arrayElement])) {
+      return arrayElement;
+    }
+  }
+};
+
+// TEST ONE
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { moonName: 'Victorio' },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.moonName === 'Victorio');
+
+assertEqual(result1, 'noma');
+
+//TEST TWO
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+assertEqual(result2, 'noma');
+
+//TEST THREE
+const result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 5 }
+}, x => x.stars === 5);
+
+assertEqual(result3, 'Akelarre');
