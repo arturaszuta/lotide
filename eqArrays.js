4b3dbe7da8dd04cf1de3163c7147a61752588346
Arturas Zuta <arturaszuta@gmail.com>
2019-07-26 16:33:14 +0000

    Add recursion to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,15 +8,19 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let longestArray = 0;
-  if (array1.length >= array2.length) {
-    longestArray = array1;
-  } else {
-    longestArray = array2;
+  console.log(array1.length + '    ' + array2.length);
+  if (array1.length !== array2.length) {
+    return false;
   }
-  for (let i = 0; i < longestArray.length; i++) {
-    if ( array1[i] !== array2[i]) {
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] && (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))) {
       return false;
+    }
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
     }
   }
   return true;
@@ -32,5 +36,9 @@ const eqArrays = function(array1, array2) {
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);  // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
 
 module.exports = eqArrays;
